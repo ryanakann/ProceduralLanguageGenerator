f4646c0b854741d70508b6807a65629e71b7c4f2
Ryan Kann <kannryan@amazon.com>
2024-06-08 18:05:08 -0400

    very dumb generate command

--- src/commands/generate.py
+++ src/commands/generate.py
@@ -1,6 +1,38 @@
 from src.command import Command
+from data.ira_sounds import IRA_SOUNDS
+from data.words import WORDS
+import re
+from random import randint
+import json
+import os
 
 class GenerateCommand(Command):
     def __init__(self, parser):
         super().__init__(parser)
-        print("Running Generate")
+        self.output = {}
+        self.generate()
+        self.write_language_to_file(self.output)
+
+    # This is a really naive and goofy "language" generator.
+    # Just getting some code out there, will enhance a LOT from here.
+    # Does not obey sonority sequencing and does not preserve like-words.
+    # For instance, "pen" might map to "vdiunth" and "pencil" to "pqttiot". Pretty bad.
+    def generate(self):
+        for word in WORDS:
+            # I don't want to deal with goofy words at the moment
+            # Filter out everything non-alphabetical
+            if re.match("^[a-z]+$", word) is not None:
+                length = randint(1, 10)
+                new_word = ""
+                for _ in range(length):
+                    # randint is inclusive on both ends
+                    index = randint(0, len(IRA_SOUNDS) - 1)
+                    new_word += IRA_SOUNDS[index]
+                self.output[word] = new_word
+    
+    def write_language_to_file(self, output):
+        if not os.path.exists("out"):
+            os.makedirs("out")
+        with open("out/language.json", "w") as file:
+            output_string = json.dumps(output)
+            file.write(output_string)
